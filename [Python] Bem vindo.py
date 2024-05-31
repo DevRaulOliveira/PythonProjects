1e1cf979946fc7c4a52ee7dbb9e411e0fd9ce37f
Raul Oliveira <168892121+DevRaulOliveira@users.noreply.github.com>
2024-05-31 10:16:56 -0300

    Add Program

---
+++ [Python] Bem vindo.py
@@ -0,0 +1,15 @@
+# Contos do programador é um programa para desabafo sincero a respeito da saúde mental do programador
+
+nome = str(input('Qual o seu nome ou apelido?'))
+idade = int(input('Qual a sua idade?'))
+teste = str(input('Você é programador?'))
+
+if teste == 'sim' or teste == 's' or teste == 'SIM' or  teste =='S':
+    prog = str(input('Qual linguagem de programação está aprendendo?'))
+    print('Bem vindo programador!')
+    print(f'Seja bem vindo, {nome}. Atualmente voce está com {idade} anos! Aprender uma nova linguagem de programação nunca é uma atividade simples, inclusive te desejo uma boa sorte no aprendizado de {prog}!  Fico feliz por você ter aberto esse programa, é um programa simples feito Python!')
+    print('Boa sorte na sua jornada! Obrigado por participar! Bora aprender juntos, fique a vontade para mandar mensagem e seguir nas redes sociais!')
+else:
+    print('Bem vindo visitante!')
+    print(f'Seja bem vindo, {nome}. Atualmente voce está com {idade} anos! Aprender uma nova linguagem de programação nunca é uma atividade simples, inclusive te convido a conhecer esse mundo incrível que está cada vez mais presente no dia a dia. Não é necessário ser exelente em matematica para começar a programar! Basta começar a praticar e ser consistente nos estudos que aos poucos os resultados aparecem!')
+    print('Boa sorte na sua jornada! Obrigado por participar! Bora aprender juntos, fique a vontade para mandar mensagem e seguir nas redes sociais!')
